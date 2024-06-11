e4c49cf5c871be28cec417ee537749269f19b171
535210091 <novita.535210091@stu.untar.ac.id>
2024-06-12 01:45:13 +0700

    Add code for EditProfileActivity

--- app/src/main/java/com/capstone/psyheart/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/com/capstone/psyheart/ui/profile/EditProfileActivity.kt
@@ -1,21 +1,40 @@
 package com.capstone.psyheart.ui.profile
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.psyheart.R
+import com.capstone.psyheart.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setupViews()
+    }
+
+    private fun setupViews() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.icBack.setOnClickListener {
+            onBackPressed()
+        }
+
+        binding.tvSaveChanges.setOnClickListener {
+            // Handle save changes logic here
         }
     }
 }
