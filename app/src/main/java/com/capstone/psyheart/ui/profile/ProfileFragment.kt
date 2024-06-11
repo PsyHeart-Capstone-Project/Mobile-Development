610388b21e390a510a88956d8f9e2b894f067a28
535210091 <novita.535210091@stu.untar.ac.id>
2024-06-12 01:46:29 +0700

    Update code for EditProfileActivity an LogoutActivity

--- app/src/main/java/com/capstone/psyheart/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstone/psyheart/ui/profile/ProfileFragment.kt
@@ -1,38 +1,42 @@
 package com.capstone.psyheart.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.capstone.psyheart.databinding.FragmentProfileBinding
+import com.capstone.psyheart.ui.logout.LogoutActivity
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this)[ProfileViewModel::class.java]
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        /*val textView: TextView = binding.profileTitleText
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }*/
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        binding.editAcc.setOnClickListener {
+            val intent = Intent(requireContext(), EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.logoutTextView.setOnClickListener {
+            val intent = Intent(requireContext(), LogoutActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
